5358074388c07d66473f6e3af991b13e5b4abed5
Morten Karlsen <eaamkarls@students.eaaa.dk>
2022-11-21 09:27:45 +0100

    Firebase config

--- BookingServer.js
+++ BookingServer.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const app = express();
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBaGDp_bVIdiYsObgZ0sGX2SkJqNDeTXGw",
+  authDomain: "testservermortens.firebaseapp.com",
+  databaseURL: "https://testservermortens-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "testservermortens",
+  storageBucket: "testservermortens.appspot.com",
+  messagingSenderId: "381122777489",
+  appId: "1:381122777489:web:4e799dc3a63fb85f46ca30"
+};
+
+// Initialize Firebase
+const firebase_app = initializeApp(firebaseConfig);
 
 //getRequest
 
