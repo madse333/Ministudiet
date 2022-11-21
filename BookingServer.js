793bca3e0df8a73756ba6e9ccb2d49f90b5499d9
Mathias <tidspe@gmail.com>
2022-11-21 10:54:30 +0100

    Working express server

--- BookingServer.js
+++ BookingServer.js
@@ -1,6 +1,12 @@
 import express from 'express';      
 const app = express();
 
+import path from 'path';
+import { fileURLToPath } from "url";
+
+const _filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(_filename);
+
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, '/views'));
 
@@ -15,22 +21,22 @@ import { getFirestore, collection, getDocs, doc, deleteDoc, addDoc, getDoc, quer
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyC7nHsiYRk1s3UqZsBZu_CwAu1iL936_18",
-  authDomain: "ministudietbooking.firebaseapp.com",
-  projectId: "ministudietbooking",
-  storageBucket: "ministudietbooking.appspot.com",
-  messagingSenderId: "759219394404",
-  appId: "1:759219394404:web:a167b94942151ad4658c71",
-  measurementId: "G-J5P8WL1TKV"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyC7nHsiYRk1s3UqZsBZu_CwAu1iL936_18",
+//   authDomain: "ministudietbooking.firebaseapp.com",
+//   projectId: "ministudietbooking",
+//   storageBucket: "ministudietbooking.appspot.com",
+//   messagingSenderId: "759219394404",
+//   appId: "1:759219394404:web:a167b94942151ad4658c71",
+//   measurementId: "G-J5P8WL1TKV"
+// };
 
 // testfeks
 
 // Initialize Firebase
-const firebase_app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const db = getFirestore(firebase_app);
+// const firebase_app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+// const db = getFirestore(firebase_app);
 
 //endpoints
 
@@ -46,3 +52,4 @@ app.get('/kalender', async (request, response) => {
 
 //deleteRequest
 
+app.listen(8080, ()=>console.log('Lytter nu på port 8080'));
