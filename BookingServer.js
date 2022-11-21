dcf6779d6c6c891f3bdc39649fdac82a6bb24ce1
Morten Karlsen <eaamkarls@students.eaaa.dk>
2022-11-21 10:00:53 +0100

    Firebase 2.0

--- BookingServer.js
+++ BookingServer.js
@@ -9,25 +9,27 @@ app.use(express.urlencoded({ extended: true }));
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getFirestore, collection, getDocs, doc, deleteDoc, addDoc, getDoc, query, where } from 'firebase/firestore'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBaGDp_bVIdiYsObgZ0sGX2SkJqNDeTXGw",
-  authDomain: "testservermortens.firebaseapp.com",
-  databaseURL: "https://testservermortens-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "testservermortens",
-  storageBucket: "testservermortens.appspot.com",
-  messagingSenderId: "381122777489",
-  appId: "1:381122777489:web:4e799dc3a63fb85f46ca30"
+  apiKey: "AIzaSyC7nHsiYRk1s3UqZsBZu_CwAu1iL936_18",
+  authDomain: "ministudietbooking.firebaseapp.com",
+  projectId: "ministudietbooking",
+  storageBucket: "ministudietbooking.appspot.com",
+  messagingSenderId: "759219394404",
+  appId: "1:759219394404:web:a167b94942151ad4658c71",
+  measurementId: "G-J5P8WL1TKV"
 };
 
 // testfeks
 
 // Initialize Firebase
 const firebase_app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const db = getFirestore(firebase_app);
 //getRequest
 
