b6aac5bb5b825fce70e830cdf2db0969eb9e28f1
Mathias <tidspe@gmail.com>
2022-11-21 11:07:40 +0100

    Firebase iimplemented

--- BookingServer.js
+++ BookingServer.js
@@ -21,27 +21,27 @@ import { getFirestore, collection, getDocs, doc, deleteDoc, addDoc, getDoc, quer
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyC7nHsiYRk1s3UqZsBZu_CwAu1iL936_18",
-//   authDomain: "ministudietbooking.firebaseapp.com",
-//   projectId: "ministudietbooking",
-//   storageBucket: "ministudietbooking.appspot.com",
-//   messagingSenderId: "759219394404",
-//   appId: "1:759219394404:web:a167b94942151ad4658c71",
-//   measurementId: "G-J5P8WL1TKV"
-// };
+const firebaseConfig = {
+  apiKey: "AIzaSyC7nHsiYRk1s3UqZsBZu_CwAu1iL936_18",
+  authDomain: "ministudietbooking.firebaseapp.com",
+  projectId: "ministudietbooking",
+  storageBucket: "ministudietbooking.appspot.com",
+  messagingSenderId: "759219394404",
+  appId: "1:759219394404:web:a167b94942151ad4658c71",
+  measurementId: "G-J5P8WL1TKV"
+};
 
 // testfeks
 
 // Initialize Firebase
-// const firebase_app = initializeApp(firebaseConfig);
+const firebase_app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
-// const db = getFirestore(firebase_app);
+const db = getFirestore(firebase_app);
 
 //endpoints
 
 //getRequest
-app.get('/kalender', async (request, response) => {
+app.get('/', async (request, response) => {
   response.render('kalender');
 })
 
