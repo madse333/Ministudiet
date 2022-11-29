36d478547a4524bda970bf4ac471b44f350c78e6
MortenHoeegJensen <morten.hoeeg.jensen@gmail.com>
2022-11-29 09:40:01 +0100

    Booktid

--- BookingServer.js
+++ BookingServer.js
@@ -151,17 +151,21 @@ async function addDokument(collectionNavn, dokumentID, data){
 let buuuuh = {navn : "John"};
 
 // PO ønsker at kunden kan vælge en ledig tid og booke den (ADD SKABELON)
-// BRUG ARRAY TIL DATO ELLER STRING TIL DATO?
-async function bookTid(kundeNavn, mail, telefonnummer, type) {
+// Datoer består af array
+async function bookTid(kundeNavn, mail, telefonnummer, type, datoStart, datoSlut, lokation) {
 
   const docRef = await addDoc(collection(firesbase_db, "tider" ), {
     kundeNavn: kundeNavn,
     mail: mail,
     telefonnummer: telefonnummer,
-    type: type
+    type: type,
+    datoStart : datoStart,
+    datoSlut : datoSlut,
+    lokation : lokation
   });
 }
 
+bookTid("John", "John@gmail.com", "12345678", "Par", [15, 12, 2022, 1200], [15, 12, 2022, 1300])
 
 //Koden viser priserne i en liste - KUN FOR FAMILIE OG PAR
 async function chooseProductsFamilieOgPar(){
