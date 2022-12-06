8836ac318e5a3b6b68241bd47d9f6ec3065fa72b
Lasse Høeg Jensen <eaalashj@students.eaaa.dk>
2022-12-06 10:17:09 +0100

    Slettet database oprettelse i flere filer

--- statistik.js
+++ statistik.js
@@ -1,30 +1,4 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getFirestore, collection, getDocs, updateDoc, doc, deleteDoc, addDoc, getDoc, query, where, setDoc } from 'firebase/firestore'
-import { async } from '@firebase/util';
-import { stringify } from 'querystring';
-import { exit } from 'process';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyC7nHsiYRk1s3UqZsBZu_CwAu1iL936_18",
-  authDomain: "ministudietbooking.firebaseapp.com",
-  projectId: "ministudietbooking",
-  storageBucket: "ministudietbooking.appspot.com",
-  messagingSenderId: "759219394404",
-  appId: "1:759219394404:web:a167b94942151ad4658c71",
-  measurementId: "G-J5P8WL1TKV"
-};
-
-// app.use(sessions({ secret: 'hemmelig', saveUninitialized: true, cookie: { maxAge: 1000*60*20 }, resave: false }));
-// Initialize Firebase
-const firebase_app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-export const firebase_db = getFirestore(firebase_app);
-
+import * as UtilsDatabase from './Database.js';
 
 let bookingerForMd = new Array(50); 	
 let count = 0;
@@ -40,18 +14,11 @@ function getCount() {
 	return count;
 }
 
-// function resetCount() {
-// 	count = 1;
-// }
-
-      
-//import { collection, query, getDocs, doc } from "firebase/firestore";
-//import { firebase_db } from "../Database";
 
 export async function getAntal(måned, produkttype) {	
     let count = 0;	
     
-    let q = query(collection(firebase_db, "tider"));
+    let q = query(collection(UtilsDatabase.firebase_db, "tider"));
     let querySnapshot = await getDocs(q);
 
     querySnapshot.forEach((doc) => {
@@ -69,7 +36,7 @@ export async function getAntal(måned, produkttype) {
 export async function getSamletTid(måned, produkttype) {		
     let tidCount = 0;	
 
-    const q = query(collection(firebase_db, "tider"));
+    const q = query(collection(UtilsDatabase.firebase_db, "tider"));
     const querySnapshot = await getDocs(q);
 
     querySnapshot.forEach((doc) => {
