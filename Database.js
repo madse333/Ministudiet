5ec4ef52070656d6396c02a72b0cc7db241e90d0
KristinaHK <kristina.h.k@hotmail.com>
2022-12-07 10:51:28 +0100

    DataBase.js og Statistik-js opdateret (virker)

--- Database.js
+++ Database.js
@@ -124,32 +124,7 @@ export async function bookTid(kundeNavn, mail, telefonnummer, type, datoStart, d
     return bookingListe;
   }
 
-  export async function getAntal(måned, produkttype) {	
-    let count = 0;	
-    let bookinger = await getCollection('tider');
 
-    bookinger.forEach((doc) => {
-          let month = doc.data().datoStart[1]
-          if (month == måned && produkttype == doc.get('type')){
-        count++;
-      }      
-    })
-    return count;
+export function getFraDb(){
+  return getCollection('tider');
 }
-
-// For at kalde getAntal fra statistik.js: console.log(getAntal);
-// For at kalde getAntal fra BookingServer.js: console.log(Utils2.getAntal);
-
-export async function getSamletTid(måned, produkttype) {		
-    let tidCount = 0;	
-    let bookinger = await getCollection('tider');
-
-    bookinger.forEach((doc) => {
-        let month = doc.data().datoStart[1]
-        if (month == måned && produkttype == doc.get('type')){
-            let tidsforbrug = doc.get('tidMin');
-            tidCount += tidsforbrug;
-    }})
-    return tidCount;    
-}
-
